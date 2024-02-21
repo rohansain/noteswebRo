32e47db9608d15a1e2c254923352d7622117552a
rohansain <88272210+rohansain@users.noreply.github.com>
2024-02-21 12:34:31 +0530

    Update notes.js
    just fixed some bugs

--- notes.js
+++ notes.js
@@ -1,6 +1,5 @@
-// let arr=[];
-console.log("i am attached");
 
+show();//to display items when page loaded first time
 function show() {
     if (localStorage.getItem("notes") != null) {
         getarrstr = localStorage.getItem("notes");
@@ -17,7 +16,7 @@ function show() {
         });
         table_body = document.getElementById("notlist");
 
-        table_body.innerHTML = sett
+        table_body.innerHTML = sett;
 
 
         //if we delete everything then it should be displayed so we use this function to check this specility make it comment and try to delete list element you see that last element delete but show
@@ -28,7 +27,6 @@ function show() {
 }
 
 function adac() {
-    // console.log("hello");
     let title = document.getElementById("title");
     let notes = document.getElementById("description");
     let title_color = document.getElementById("title_color");
@@ -38,7 +36,7 @@ function adac() {
 
         if (localStorage.getItem("notes") == null) {
             arr = [];
-            arr.push([title.value, notes.value]);
+            arr.push([title.value, notes.value,title_color.value,notes_color.value]);
             localStorage.setItem("notes", JSON.stringify(arr));
         }
         else {
@@ -47,98 +45,26 @@ function adac() {
             arr.push([title.value, notes.value,title_color.value,notes_color.value]);
             localStorage.setItem("notes", JSON.stringify(arr));
         }
-
-        tbo = document.getElementById("notlist");
-        // sett = "";
-        let sett="";
-
-        arr.forEach((element, index) => {
-            sett +=`<tr>
-            <td>${index + 1}</td>
-            <td style="color:${element[2]}">${element[0]}</td>
-            <td style="color:${element[3]}">${element[1]}</td>
-            <td class="del_btn_change"><input type="button" id="del_btn"  onclick="deleted(${index})"  value="delete"></td>
-            </tr>`
-        });
-
-        if(sett!=""){
-            table_body.innerHTML = sett;
-        }
-        //console.log(table_body);
-        // console.log(table_body.innerHTML);
         title.value = "";
         notes.value = "";
         show();
     }
 }
+
 let as = document.getElementById("addBtn");
 as.addEventListener("click", adac);
-show();
 function deleted(index) {
-    // console.log("i am here");
     getarrstr = localStorage.getItem("notes");
 
     arr = JSON.parse(getarrstr);
-    console.log(arr);
+    // console.log(arr);
     arr.splice(index, 1);
-    console.log(arr);
+    // console.log(arr);
     localStorage.setItem("notes", JSON.stringify(arr));
-    adac();
+    // adac();
     show();
     
     console.log("delete this", index);
 }
 
-    // let insertintbo;
-    // let arrayforvalue=[];
-    // function insert() {
-        //     title = document.getElementById("title");
-        //     desc = document.getElementById("description");
-        //     arrayforvalue = [];
-        //     arrayforvalue.push(title.value, desc.value);
-//     localStorage.setItem("notes", JSON.stringify(arrayforvalue));
-//     title.value = " ";
-//     desc.value = " ";
-
-
-//     let listfornotes = document.getElementsByClassName("notlist");
-//     let gettr="";
-//     arrayforvalue.forEach((element, index) => {
-    //         gettr+= `<tr>
-    //         <td>${index+1}</td>
-    //         <td>${element}</td>
-    //         <td>${element}</td>
-    //         <td><input type="button" value="delete"></td>
-    //         </tr>`
-    //     });
-//     // console.log(arrayforvalue);
-//     console.log(gettr);
-//     listfornotes.innerHTML = gettr;
-// }
-
-// let addBtn = document.getElementById("addBtn");
-// addBtn.addEventListener("click", insert);
-
-
-//practise
-
-//     if(tbo.innerHTML==null){
-//    insertintbo=`<tr>
-//         <td>1</td>
-//         <td>${title.value}</td>
-//         <td>${notes.value}</td>
-//         <td><input type="button" value="delete"></td>
-//         </tr>`
-//     }
-//     else{
-//         insertintbo+=`<tr>
-//         <td>1</td>
-//         <td>${title.value}</td>
-//         <td>${notes.value}</td>
-//         <td><input type="button" value="delete"></td>
-//         </tr>`
-//     }
-    // tbo.innerHTML=insertintbo;
-    // title.value = "";
-    // notes.value= "";
-    // console.log(tbo);
+    
